5556e200e462487308e0c557f987e9841e3f2a70
Daniel <dmmodestor@ut.edu.co>
2025-06-18 18:13:06 -0500

    Más configuracion para Render y Railway V2

--- app.py
+++ app.py
@@ -7,12 +7,14 @@ app = Flask(__name__)
 app.secret_key = 'clave_secreta'
 
 host = os.getenv("DB_HOST")
+port = int(os.getenv("DB_PORT"))
 user = os.getenv("DB_USER")
 password = os.getenv("DB_PASSWORD")
 database = os.getenv("DB_NAME")
 
 db = mysql.connector.connect(
     host=host,
+    port=port,
     user=user,
     password=password,
     database=database
